7007b8977164b4882140a5fce90f9c5545098e5c
RinieS <ishlapak72@gmail.com>
2023-04-20 08:06:45 -0600

    task 1a(router, controller)

--- routes/menuRouter.js
+++ routes/menuRouter.js
@@ -4,6 +4,9 @@ const menuController = require("../controllers/menuController");
 const menuRouter = Router();
 
 menuRouter.get("/", menuController.getAll);
+// task 1a
+menuRouter.put("/:id", menuController.update);
+
 menuRouter.get("/:id", menuController.getOne);
 menuRouter.post("/", menuController.create);
 
